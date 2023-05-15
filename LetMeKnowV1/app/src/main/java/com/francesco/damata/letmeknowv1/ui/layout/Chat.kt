28833b73d0d803f2e56acc359fa67d908575aa02
Frenk202 <francescodamata@gmail.com>
2023-05-15 18:25:08 +0200

    in fase aggiornamento chat

--- LetMeKnowV1/app/src/main/java/com/francesco/damata/letmeknowv1/ui/layout/Chat.kt
+++ LetMeKnowV1/app/src/main/java/com/francesco/damata/letmeknowv1/ui/layout/Chat.kt
@@ -1,16 +1,18 @@
 package com.francesco.damata.letmeknowv1.ui.layout
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -20,13 +22,21 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.francesco.damata.letmeknowv1.R
 import com.francesco.damata.letmeknowv1.screen.MyModelScreen
-import com.francesco.damata.letmeknowv1.ui.theme.letMeKnowColor
+import com.francesco.damata.letmeknowv1.ui.theme.recived
+import com.francesco.damata.letmeknowv1.ui.theme.sended
 
 @Composable
 fun Chat(myModelScreen: MyModelScreen) {
-    Column(){
+    val MessageListObs=rememberSaveable() {
+        mutableStateOf(MessageListChat)
+    }
+    Column (modifier=Modifier
+        .verticalScroll(rememberScrollState())
+    ){
+
         TopAppBar(
             {
                 IconButton({
@@ -34,40 +44,35 @@ fun Chat(myModelScreen: MyModelScreen) {
                 }) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button),
+                        modifier=Modifier .padding(start = 5.dp)
+                    )
+                }
+                IconButton({
+                    ScreenRouter.navigateTo(LetMeKnowScreen.RecentChat)
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
                         contentDescription = stringResource(R.string.back_button)
                     )
                 }
-                IconButton(onClick = {
-                    //ScreenRouter.navigateTo(LetMeKnowScreen.Visit)
 
-                },
-                    modifier = Modifier
-                        .then(Modifier.size(70.dp))
-                        .border(5.dp, MaterialTheme.colors.letMeKnowColor, shape = CircleShape)
-                ) {
-                    Icon(Icons.Default.Person,
-                        contentDescription = "content description",
-                        tint = "#8290f1".color,
-                        modifier=Modifier
-                            .size(100.dp)
-                    )
-                }
                 Text(
-                    "User #Jessie", color = Color.White, fontSize = 24.sp,
+                    text = stringResource(R.string.user) +"#123456", color = Color.White, fontSize = 24.sp,
                     modifier = Modifier
                         .align(Alignment.Start)
                         .padding(start = 20.dp, top = 10.dp)
                 )
             }
         )
-        Conversation(MessageListChat)
-
+        Conversation(MessageListObs.value)
+        ChatBar(MessageListObs)
     }
 }
 @Composable
 fun Conversation(messages: List<Message>) {
     LazyColumn(
-        modifier=Modifier.height(500.dp),
+        modifier=Modifier.height(600.dp),
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         items(items=messages){
@@ -81,44 +86,76 @@ fun MessageChat(message:Message, user:String, sender:String){
     var msgExpanded= rememberSaveable() {
         mutableStateOf(false)
     }
-    Row{
-        if (message.sender==user) {
-            Spacer(modifier = Modifier.width(100.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Spacer(modifier = Modifier.width(40.dp))
-                Text(
-                    message.reciver,
-                    style = MaterialTheme.typography.body1,
-                    fontSize = 24.sp,
-                    maxLines = if (msgExpanded.value) Int.MAX_VALUE else 5,
-                    textAlign = TextAlign.End,
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .width(300.dp)
+    ConstraintLayout() {
+        val (row,Surface)=createRefs()
+        Row{
+            if (message.sender==user) {
+                Spacer(modifier = Modifier.width(100.dp))
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Spacer(modifier = Modifier.width(40.dp))
+                    Text(
+                        message.reciver,
+                        style = MaterialTheme.typography.body1,
+                        fontSize = 24.sp,
+                        maxLines = if (msgExpanded.value) Int.MAX_VALUE else 5,
+                        textAlign = TextAlign.End,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.sended)
+                            .width(300.dp)
 
 
-                )
+                    )
+                }
             }
-        }
-        else{
+            else{
 
                 Surface(shape=MaterialTheme.shapes.medium,elevation =1.dp){
-                Text(message.reciver,
-                    style=MaterialTheme.typography.body1,
-                    fontSize = 24.sp,
-                    maxLines=if(msgExpanded.value)Int.MAX_VALUE else 5,
-                    modifier=Modifier
-                        .padding(start=10.dp)
-                        .background(Color.Red)
-                        .widthIn(100.dp,300.dp)
-                )
+                    Text(message.reciver,
+                        style=MaterialTheme.typography.body1,
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        maxLines=if(msgExpanded.value)Int.MAX_VALUE else 5,
+                        modifier= Modifier
+                            .padding(start = 10.dp)
+                            .background(MaterialTheme.colors.recived)
+                            .widthIn(100.dp, 300.dp)
+                    )
+                }
+
             }
 
         }
-
     }
+
 }
-val MessageListChat=listOf(
+@Composable
+fun ChatBar(MessageListObs: MutableState<MutableList<Message>>) {
+    val inputMsg = rememberSaveable() {
+        mutableStateOf("")
+    }
+    TextField(value = inputMsg.value,
+        onValueChange = {
+           inputMsg.value=it
+        },
+
+        textStyle = LocalTextStyle.current.copy(fontSize = 32.sp),
+        modifier=Modifier.fillMaxWidth(),
+        trailingIcon={
+            IconButton(onClick={
+                var msg:Message
+                msg=Message ("Francesco" , inputMsg.value,"Jessie")
+                MessageListObs.value.add(msg)
+            }){
+                Icon(
+                    imageVector = Icons.Default.Send,//https://fonts.google.com/icons
+                    contentDescription =stringResource(R.string.sendMessage)//in there you can find into your messages
+                )
+            }
+        })
+
+}
+val MessageListChat= mutableListOf<Message>(
     Message("Francesco","Really exited","Jessie"),
     Message("Jessie","Really exitedQ4VAEWFGWSFGVGW4STRWHTRSGHWT4ASEFVTG5QAERVTQR3EAVGQAEGQAEFGQAGRQACEQAERCQAEFGQAEFCGQAERGRQEAF","Francesco"),
     Message("Jessie","Really exited","Francesco"),
@@ -127,9 +164,3 @@ val MessageListChat=listOf(
     Message("Jessie","Really exitedQ4VAEWFGWSFGVGW4STRWHTRSGHWT4ASEFVTG5QAERVTQR3EAVGQAEGQAEFGQAGRQACEQAERCQAEFGQAEFCGQAERGRQEAF","Francesco"),
     Message("Francesco","EFQxCAEFZVHTWGRSVTBW4TSEREY5HREY5HDSTRYEYDTBEYTGD3E5TRW54TRS3V5TERTW3REVTWGRHETGJHYJEBWSTRVZWESAEAECREWZS","Jessie"),
 )
-@Composable
-fun Bottom(messages: List<Message>) {
-    Row(){
-
-    }
-}
--- LetMeKnowV1/app/src/main/java/com/francesco/damata/letmeknowv1/ui/theme/Color.kt
+++ LetMeKnowV1/app/src/main/java/com/francesco/damata/letmeknowv1/ui/theme/Color.kt
@@ -8,10 +8,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-//LOGIN
 val Colors.myBlue
     get()="#377DFF".color
 val Colors.letMeKnowColor
     get()="#387eff".color
 val Colors.button
+    get()="#377DFF".color
+val Colors.recived
+    get()="#D6D6D6".color
+val Colors.sended
     get()="#377DFF".color
